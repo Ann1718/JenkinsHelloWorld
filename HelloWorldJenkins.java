dd32c21a0a4673dfafc325f18e20c6891260a133
Ann1718 <87471544+Ann1718@users.noreply.github.com>
2021-09-15 15:23:15 +0800

    Update HelloWorldJenkins.java

--- HelloWorldJenkins.java
+++ HelloWorldJenkins.java
@@ -3,7 +3,7 @@ package HelloWorldJenkins;
 public class HelloWorldJenkins {
 
 	public static void main(String[] args) {
-		System.out.println("---***Hello World & Welcome to Jenkins Training***---");
+		System.out.println("---***Made some change***---");
 
 	}
 
